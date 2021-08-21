61ba0868a6d51218e8f5b6ccae7373fe7dce530a
Cameron Mahaffey <78636033+CameronMahaffey@users.noreply.github.com>
2021-08-21 15:14:02 -0400

    Update pgm_convert_and_resize.py

--- pgm_convert_and_resize.py
+++ pgm_convert_and_resize.py
@@ -5,11 +5,53 @@ from PIL import Image
 import numpy as np
 
 
-def converter(image):
+# Ensure that the file is a P2 .PGM, encoded in LF format (i.e. '\n' newline)
+def image_check(image_file):
+    # print(image_file)
+    get_line = image_file.readline()
+
+    try:
+        assert get_line[0] == 'P'
+        assert get_line[1] == '2'
+        assert get_line[2] == '\n'
+    except AssertionError:
+        print('Error. Must be a \'P2\' PGM file. Make sure the file is properly encoded in \'LF\' format')
+        quit()
+
+    get_line = image_file.readline()  # skip comments
+    while get_line[0] == '#':
+        get_line = image_file.readline()
+
+    try:
+        assert get_line[0] == '9'
+        assert get_line[1] == '0'
+        assert get_line[2] == '9'
+    except AssertionError:
+        print('Error. Image must have a width of 909 pixels.')
+        quit()
+    image_height = int(get_line[3:])
+    image_file.readline()
+    image_file = np.array(image_file.read().split('\n'))
+    print(image_file)
+    return image_file, image_height
+
+
+def converter(image_file_path):
+    image_width = 0
+    image_height = 0
+    im = None
     # Open image, convert to greyscale, check width and resize if necessary
-    im = Image.open(f'input/{image}')
-    im = im.convert("L")
-    image_width, image_height = im.size
+    if image_file_path.lower().endswith(('.png', '.jpg')):
+        print("Not a PGM, converting...")
+        im = Image.open(f'{image_file_path}')
+        im = im.convert("L")
+        image_width, image_height = im.size
+    elif image_file_path.lower().endswith('.pgm'):
+        print("PGM file, no conversion needed.")
+        return image_check(open(f'{image_file_path}'))
+    else:
+        print("Error. Image must be a .pgm, .png, or .jpg extension")
+        quit()
     # image_array = np.array(im)
     print(f"Size: {im.size}")      # Mode: {im.mode}")
     # im.show()
@@ -20,33 +62,17 @@ def converter(image):
         new_height = int((float(image_height) * float(ratio)))
         im = im.resize((new_width, new_height))
         print(f"New Size: {im.size}")
-
+    image_width, image_height = im.size
     # Save image data in a numpy array and make it 1D.
     image_array1 = np.array(im).ravel()
-    # print(f"Picture Array: {image_array1}")
+    # print(f"Picture Array: {image_array1} size: {image_array1.size}")
 
-    print(f"Saving as PGM...")
-    # Create file w .pgm ext to store data in, first 4 lines are: pgm type, comment, image size, maxVal
-    new_pgm_file = open("input/_created_by_temp_converter.pgm", "w+")
-    new_pgm_file.write("P2\n# created by pgm_convert_and_resize.py \n%d %d\n255\n" % im.size)
+    print(f"Converting to PGM...")
+    new_pgm_file = []
 
-    # Storing greyscale data in file with \n delimiter
+    # Storing greyscale data in array with \n delimiter
     for number in image_array1:
-        new_pgm_file.write(str(number) + '\n')
-    new_pgm_file.close()
-    # im = im.save(r"pics/IMG_0277-greyscale.jpg")
+        new_pgm_file.append(number)
+    print("Finished conversion")
 
-    # replacement strings
-    windows_line_ending = b'\r\n'
-    unix_line_ending = b'\n'
-
-    # small code snippet that changes a Windows encoded text file into a Unix encoded one, or nothing if not.
-    with open('input/_created_by_temp_converter.pgm', 'rb') as new_pgm_file:
-        content = new_pgm_file.read()
-
-    content = content.replace(windows_line_ending, unix_line_ending)
-
-    with open('input/_created_by_temp_converter.pgm', 'wb') as new_pgm_file:
-        new_pgm_file.write(content)
-    new_pgm_file.close()
-    print(f"Finished")
+    return new_pgm_file, image_height
