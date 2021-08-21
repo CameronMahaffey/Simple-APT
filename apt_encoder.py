f2aa127f893abc6b5ebaf0a78ad1f98c716a01b4
Cameron Mahaffey <78636033+CameronMahaffey@users.noreply.github.com>
2021-08-21 15:13:30 -0400

    Update apt_encoder.py

--- apt_encoder.py
+++ apt_encoder.py
@@ -20,51 +20,14 @@ def get_pixel_position(row, col):
     return col * apt_width + row
 
 
-# Ensure that the file is a P2 .PGM, encoded in LF format (i.e. '\n' newline)
-def image_load_and_check(file_name):
-    image_file = open(f'input/{file_name}', 'r')
-    get_line = image_file.readline()
-
-    try:
-        assert get_line[0] == 'P'
-        assert get_line[1] == '2'
-        assert get_line[2] == '\n'
-    except AssertionError:
-        print('Error. Must be a \'P2\' PGM file. Make sure the file is properly encoded in \'LF\' format')
-        quit()
-
-    get_line = image_file.readline()  # skip comments
-    while get_line[0] == '#':
-        get_line = image_file.readline()
-
-    try:
-        assert get_line[0] == '9'
-        assert get_line[1] == '0'
-        assert get_line[2] == '9'
-    except AssertionError:
-        print('Error. Image must have a width of 909 pixels.')
-        quit()
-    image_file.readline()
-    image_file = np.array(image_file.read().split('\n'))
-    return image_file
-
-
-# Start of the program, load image and make sure it is properly formatted for encoding
+# Start of the program, load image arg and send to converter to make sure it is pgm p2, 909 pixels wide
 input_file_name = sys.argv[1]
-
-if input_file_name.lower().endswith(('.png', '.jpg', '.jpeg')):
-    print("Not a PGM, converting...")
-    the_pgm_image = pgm_convert_and_resize.converter(input_file_name)
-    the_pgm_image = image_load_and_check('_created_by_temp_converter.pgm')
-elif input_file_name.lower().endswith('.pgm'):
-    print("PGM file, no conversion needed. (must be 909 pixels wide)")
-    the_pgm_image = image_load_and_check(input_file_name)
-else:
-    print("Error. Image must be a .pgm, .png, .jpg, or .jpeg extension")
-    quit()
-
-image_height = the_pgm_image.size // apt_width
-print("APT encoding started..")
+output_file_name = sys.argv[2]
+the_pgm_image, image_height = pgm_convert_and_resize.converter(input_file_name)
+# the_pgm_image = np.int16(the_pgm_image)
+# print(f"the pgm image = {the_pgm_image}")
+# print(f"image_height = {image_height}")
+# print("APT encoding started..")
 
 # Main loop, sending both pictures and sync/telemetry 1 row at a time, till the height of the picture as been reached.
 for rows in range(image_height):
@@ -125,6 +88,7 @@ for rows in range(image_height):
         stored_apt_pic.append(v)
 
 stored_apt_pic = np.float64(stored_apt_pic)
+# print(f'stored_apt_pic: {stored_apt_pic}')
 
 # Create the sine carrier sampling array with a length equal to the image array (total pixels)
 sine_carrier = np.sin(carrier_hz * 2.0 * np.pi * np.arange(len(stored_apt_pic)) / baud_rate)
@@ -133,14 +97,5 @@ sine_carrier = np.sin(carrier_hz * 2.0 * np.pi * np.arange(len(stored_apt_pic)) 
 audio_sample = np.int16(sine_carrier * stored_apt_pic * 48)
 
 # Save the encoded data into a .wav file
-wav_file_name = input_file_name[:-4]
-wav.write(f'wav/{wav_file_name}.wav', baud_rate, audio_sample)
-print(f"Finished, .wav file saved in Simple-APT/wav/{wav_file_name}.wav")
-
-# # Check out the finished picture as a .pgm
-# new_pgm_file = open("pics/example-pgm.pgm", "w+")
-# new_pgm_file.write("P2\n# apt pre-modulated image \n%d %d\n255\n" % (2080, 1606))
-#
-# for pixel in stored_apt_pic:
-#     new_pgm_file.write(str(pixel) + '\n')
-# new_pgm_file.close()
+wav.write(output_file_name, baud_rate, audio_sample)
+print(f"Finished, .wav file saved in {output_file_name}")
